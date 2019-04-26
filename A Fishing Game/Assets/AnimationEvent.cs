55ff325a654e0fa0c9b0d175418b81e2e43f3f43
Tobiasnm <tnma14@student.aau.dk>
2019-04-27 00:02:00 +0200

    fiskefiskeifksiekfisk

---
+++ A Fishing Game/Assets/AnimationEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour
+{
+    public GameObject fishingLine;
+    
+    public void AniEvent()
+    {
+        FishingLine line = fishingLine.GetComponent<FishingLine>();
+        line.throwStarted = true;
+    }
+}
