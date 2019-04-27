18ab2d6044e8fcd0477d4d273e59c6bb6b12e673
Dan Tomicic <dan.tomicic@gmail.com>
2019-04-27 18:06:00 +0200

    first push lets break the git shall we

---
+++ A Fishing Game/Assets/Scripts/FishAI.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAI : MonoBehaviour
+{
+    [SerializeField]
+    float maxYPos, minYPos, maxXPos, minXPos, maxZPos, minZPos;
+
+
+
+    private float startTime;
+    public float startSpeed = 5f;
+    public float currentSpeed = 5f;
+    public float burstSpeed = 0.01f;
+    Rigidbody rb;
+    Vector3 EAV;
+    bool turning = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        transform.Rotate(new Vector3(Random.Range(-20,20), 180, 0));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    void Move()
+    {
+        rb.velocity = -transform.forward * currentSpeed;
+        turnScript();
+        if (turning == false)
+        {
+            if (Random.Range(0f, 1f) < 0.02)
+            {
+                StartCoroutine(burstOfSpeed());
+            }
+
+        }
+
+
+    }
+
+    IEnumerator burstOfSpeed()
+    {
+        currentSpeed += 5;
+        yield return new WaitForSeconds(0.2f);
+        currentSpeed = startSpeed;
+    }
+
+    void Turn(Vector3 turnVector)
+    {
+        Quaternion deltaRotation = Quaternion.Euler(turnVector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+        turning = true;
+    }
+
+    void turnScript()
+    {
+        if (transform.position.x  >= maxXPos || transform.position.z >= maxZPos)
+        {
+            EAV = new Vector3(0, 100, 0);
+
+            Turn(EAV);
+        }
+        else if (transform.position.x <= minXPos  || transform.position.z <= minZPos  )
+        {
+            EAV = new Vector3(0, 100, 0);
+            Turn(EAV);
+        }
+        else
+        {
+            EAV = new Vector3(0, 0, 0);
+            turning = false;
+        }
+
+        if( transform.position.y > maxYPos)
+        {
+            Turn(new Vector3(Random.Range(-20, -10), 0, 0));
+        }
+        else if(transform.position.y < minYPos)
+        {
+            Turn(new Vector3(Random.Range(10, 20), 0, 0));
+        }
+        else { turning = false; }
+
+    }
+
+
+    Vector3 UpOrDown()
+    {
+        if (transform.position.y > minYPos)
+        {
+            return new Vector3(Random.Range(-50, 0), 0, 0);
+        }
+        else
+        {
+            return new Vector3(Random.Range(0, 50), 0, 0);
+        }
+    }
+
+    Vector3 rightOrLeft()
+    {
+        if (Random.Range(0f, 1f) > 0.5)
+        {
+            return new Vector3(0, Random.Range(-10, 0), 0);
+        }
+        else
+        {
+            return new Vector3(0, Random.Range(0, 10), 0);
+        }
+    }
+
+}
---
+++ A Fishing Game/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler: MonoBehaviour
+
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+
+        StartCoroutine(spawn("Fish", transform.position, Quaternion.identity));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawn(string tag, Vector3 pos, Quaternion rot)
+    {
+        SpawnFromPool("Fish", transform.position, Quaternion.identity).transform.Rotate(new Vector3(0,Random.Range(-90,90),0));
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(spawn("Fish", transform.position, Quaternion.identity));
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 pos, Quaternion rot)
+    {
+
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.Log("Pool tag error");
+            return null;
+        }
+        GameObject objspawn = poolDictionary[tag].Dequeue();
+        objspawn.SetActive(true);
+        objspawn.transform.position = pos;
+        objspawn.transform.rotation = rot;
+
+        poolDictionary[tag].Enqueue(objspawn);
+
+        return objspawn;
+    }
+}
