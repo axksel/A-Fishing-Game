2de870e9c925066bd5b812b4b6de188470d20b44
axksel <jonas.aksel@gmail.com>
2019-04-27 23:07:52 +0200

    disabler

---
+++ A Fishing Game/Assets/DisablerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisablerScript : MonoBehaviour
+{
+
+    public GameObject fiskeStang;
+    public GameObject grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+
+        if(collision.gameObject.tag=="Player")
+        {
+
+            fiskeStang.SetActive(false);
+        grid.SetActive(false);
+        }
+
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            fiskeStang.SetActive(true);
+            grid.SetActive(true);
+        }
+    }
+}
