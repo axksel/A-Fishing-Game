e24fe7dba600c5b623be834181fad71a639629af
Tobiasnm <tnma14@student.aau.dk>
2019-04-28 12:31:14 +0200

    en cursor

---
+++ A Fishing Game/Assets/EnableCursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
