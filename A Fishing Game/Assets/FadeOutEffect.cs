1abba30226bcc093f6f5bb2adb63f6458298a940
NiclasRant <niclasnr@hotmail.com>
2019-04-28 11:51:41 +0200

    Cinemachine and cutscenes

---
+++ A Fishing Game/Assets/FadeOutEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOutEffect : MonoBehaviour
+{
+    public Image m_Image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Image.CrossFadeAlpha(1, 2.0f, false);
+    }
+}
